cd15c0a41c11952d854b170d2627ad5c1a29ae99
johnny <johnny@itfolks.com.au>
2014-12-31 17:55:25 +1100

    move setup to start.sh in order to use $DOMAIN name

--- Dockerfile
+++ Dockerfile
@@ -3,24 +3,7 @@ MAINTAINER Johnny Zheng <johnny@itfolks.com.au>
 
 RUN yum -y install git sendmail  && \
     yum clean all
-RUN : ${DOMAIN:=$PRESTASHOP}; PRESTASHOP=prestashop-$DOMAIN; && \
-    if [[ ! -d /data/www/$PRESTASHOP || "$OVERRIDEN" == "TRUE" ]]; then \
-        cd /data/www && \
-        git clone https://github.com/PrestaShop/PrestaShop.git $PRESTASHOP && \
-        chown www:www $PRESTASHOP -R && \
-        cd /data/www/$PRESTASHOP; chmod 775 upload config themes log cache && \
-    fi
-RUN cat > /data/conf/nginx/hosts.d/$PRESTASHOP.conf <<EOF
-server { \
-      listen      80 default; \
-      server_name $DOMAIN; \
-      root        /data/www/$DOMAIN; \
-      index       index.php index.html; \
-      include     /etc/nginx/conf.d/default-*.conf; \
-      include     /data/conf/nginx/conf.d/default-*.conf; \
-      include     /etc/nginx/conf.d/php-location.conf; \
-} \
-EOF
+
 ADD ./start.sh ~/start.sh
 RUN chmod 755 ~/start.sh
 CMD ["/bin/bash", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,5 +1,27 @@
-: ${DOMAIN:=$PRESTASHOP}; PRESTASHOP=prestashop-$DOMAIN; : ${SHOP_NAME:=$DOMAIN}; : ${LANGUAGE:="en"}; : ${TIMEZONE:="Europe/Paris"}; : ${SHOP_PASSWORD:="0123456789"}; : ${SHOP_EMAIL:="admin@$DOMAIN"};
+: ${DOMAIN:=$HOSTNAME}; PRESTASHOP=prestashop-$DOMAIN; : ${SHOP_NAME:=$DOMAIN}; : ${LANGUAGE:="en"}; : ${TIMEZONE:="Europe/Paris"}; : ${SHOP_PASSWORD:="0123456789"}; : ${SHOP_EMAIL:="admin@$DOMAIN"};
 export TZ=$TIMEZONE
+
+if [[ ! -d /data/www/$PRESTASHOP || "$OVERRIDEN" == "TRUE" ]]; then
+        cd /data/www
+        git clone https://github.com/PrestaShop/PrestaShop.git $PRESTASHOP
+        chown www:www $PRESTASHOP -R
+        cd /data/www/$PRESTASHOP; chmod 775 upload config themes log cache
+fi
+
+if [[ ! -d /data/conf/nginx/hosts.d/$DOMAIN.conf || "$OVERRIDEN" == "TRUE" ]]; then
+cat > /data/conf/nginx/hosts.d/$DOMAIN.conf <<EOF
+server {
+      listen      80 default;
+      server_name $DOMAIN;
+      root        /data/www/$PRESTASHOP;
+      index       index.php index.html;
+      include     /etc/nginx/conf.d/default-*.conf;
+      include     /data/conf/nginx/conf.d/default-*.conf;
+      include     /etc/nginx/conf.d/php-location.conf;
+}
+EOF
+fi
+
 if [[ -d /data/www/$PRESTASHOP/install-dev || "$OVERRIDEN" == "TRUE" ]]; then
     cd /data/www/$PRESTASHOP/install-dev
     php index_cli.php --domain=$DOMAIN --name=$SHOP_NAME --language=$LANGUAGE --timezone=$TIMEZONE \
